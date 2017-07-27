b5da246e5334051bafadc0977a55fde8e462dc08
Nishchay-sopho <agrawal.nishchay5@gmail.com>
2017-07-27 14:12:31 +0530

    First commit

---
+++ app/src/main/java/com/example/nishchay/loginapp/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.nishchay.loginapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView registerTextView;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+
+    private String loginURL = "http://192.168.225.223:3000/users";
+    private RequestQueue loginRequestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        registerTextView = (TextView) findViewById(R.id.registerTextView);
+        loginButton = (Button) findViewById(R.id.loginBtn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doLogin();
+            }
+        });
+
+    }
+
+    private void doLogin() {
+
+        if (loginRequestQueue == null)
+            loginRequestQueue = Volley.newRequestQueue(MainActivity.this);
+
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("username", usernameEditText.getText().toString().trim());
+        params.put("password", passwordEditText.getText().toString().trim());
+
+
+        JsonObjectRequest loginRequest = new JsonObjectRequest(loginURL, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(MainActivity.this, "Successfully logged in", Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+//                SharedPreferences sharedPreferences = getSharedPreferences("tokensFile", Context.MODE_PRIVATE);
+//                headers.put("Authorization", "Bearer " + sharedPreferences.getString("accessToken", "null"));
+                return headers;
+            }
+        };
+
+        loginRequestQueue.add(loginRequest);
+    }
+}
